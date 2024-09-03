8cab92ec1f1b225e3c2896574c07bd6add1d1e57
AtaMakesStuff <sinankit@udel.edu>
2024-09-02 21:54:54 -0400

    Did the rest

--- Homework Project/Assets/Scripts/HelloWorld.cs
+++ Homework Project/Assets/Scripts/HelloWorld.cs
@@ -7,7 +7,15 @@ public class HelloWorld : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-     Debug.Log("Hello World!");   
+     Debug.Log("Hello World!");
+     Debug.LogWarning("This is a warning!");
+     Debug.LogError("ERROR!!!");
+     // Author: Ata Kitapci
+     /*
+     Will we make a game?
+     Farming Sim but spooky
+     Implementing scripts into games
+     */  
     }
 
     // Update is called once per frame
