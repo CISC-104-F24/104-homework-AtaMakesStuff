1d46c12e4427289e02a6f4488459d5bdbd6060fb
AtaMakesStuff <sinankit@udel.edu>
2024-10-16 16:18:02 -0400

    finished part A, starting part B

--- Homework Project/Assets/Scripts/PlayerCharacter.cs
+++ Homework Project/Assets/Scripts/PlayerCharacter.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class PlayerCharacter : MonoBehaviour
 {
-    public float moveSpeed = 100f;
+    public float moveSpeed = 10f;
 
     private Rigidbody myRigidbody; 
 
@@ -18,11 +18,44 @@ public class PlayerCharacter : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        // WASD for four directional movement
         bool forwardPressed = Input.GetKey(KeyCode.W);
 
         if (forwardPressed)
         {
-            myRigidbody.AddForce(new Vector3(0f,0f,1f) * moveSpeed, ForceMode.Impulse);
+            transform.position = transform.position + transform.forward * moveSpeed * Time.deltaTime;
         }
+
+        bool backPressed = Input.GetKey(KeyCode.S);
+        
+        if (backPressed)
+        {
+            transform.position = transform.position + new Vector3(0f,0f,-1f) * moveSpeed * Time.deltaTime;
+        }
+
+        bool leftPressed = Input.GetKey(KeyCode.A);
+        
+        if (leftPressed)
+        {
+            transform.position = transform.position + new Vector3(-1f,0f,0f) * moveSpeed * Time.deltaTime;
+        }
+
+        bool rightPressed = Input.GetKey(KeyCode.D);
+
+        if (rightPressed)
+        {
+            transform.position = transform.position + new Vector3(1f,0f,0f) * moveSpeed * Time.deltaTime;
+        }
+
+        //Spacebar for jumping 
+
+        bool jumpPressed = Input.GetKeyDown(KeyCode.Space);
+
+        if (jumpPressed) 
+        {
+            
+        }
+
+        
     }
 }
