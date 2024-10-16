0f1a34d165400d6e8d1a947f9920f8961a440b48
AtaMakesStuff <sinankit@udel.edu>
2024-10-16 16:02:30 -0400

    working on part A

--- Homework Project/Assets/Scripts/PlayerCharacter.cs
+++ Homework Project/Assets/Scripts/PlayerCharacter.cs
@@ -4,15 +4,25 @@ using UnityEngine;
 
 public class PlayerCharacter : MonoBehaviour
 {
+    public float moveSpeed = 100f;
+
+    private Rigidbody myRigidbody; 
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        myRigidbody = GetComponent<Rigidbody>();
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        bool forwardPressed = Input.GetKey(KeyCode.W);
+
+        if (forwardPressed)
+        {
+            myRigidbody.AddForce(new Vector3(0f,0f,1f) * moveSpeed, ForceMode.Impulse);
+        }
     }
 }
