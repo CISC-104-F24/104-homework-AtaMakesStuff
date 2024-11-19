1e34736e5d54b2a1d351bfb21e95ea40aba5a10b
AtaMakesStuff <sinankit@udel.edu>
2024-11-19 08:31:58 -0500

    finish part B

---
+++ Homework Project/Assets/Scripts/Card.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class Card : MonoBehaviour
+{
+    public string cardName;
+
+    public int attackDamage;
+
+    public int healthPoints; 
+
+    public Image pokemon;
+
+    public TextMeshProUGUI healthDisplay;
+
+    public TextMeshProUGUI nameDisplay;
+
+    public TextMeshProUGUI attackDisplayOne;
+
+    public TextMeshProUGUI attackDisplayTwo;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
